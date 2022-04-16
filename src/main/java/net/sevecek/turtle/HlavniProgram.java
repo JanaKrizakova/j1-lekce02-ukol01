a20ce0a1fa1015b2ef34935725a6a2ce940d9437
JanaKrizakova <dvornikova.jana@gmail.com>
2022-04-16 18:40:43 +0200

    Část 1 - nakreslení prasátka přesunuto do metody nakresliPrasatko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -4,53 +4,59 @@ import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
 
+    static void nakresliPrasatko(Turtle zelva) {
+
+        zelva.penDown();
+
+        //predni nohy
+        zelva.turnLeft(135);
+        zelva.move(50);
+        zelva.turnLeft(180);
+        zelva.move(50);
+
+        zelva.turnRight(90);
+        zelva.move(50);
+        zelva.turnLeft(180);
+        zelva.move(50);
+
+        zelva.turnRight(135);
+        zelva.move(200);
+
+        //zadni nohy
+        zelva.turnRight(135);
+        zelva.move(50);
+        zelva.turnLeft(180);
+        zelva.move(50);
+
+        zelva.turnRight(90);
+        zelva.move(50);
+        zelva.turnLeft(180);
+        zelva.move(50);
+
+        zelva.turnRight(45);
+        zelva.move(100);
+
+        zelva.turnLeft(90);
+        zelva.move(200);
+
+        zelva.turnLeft(90);
+        zelva.move(100);
+
+        //hlava
+        zelva.turnRight(120);
+        zelva.move(100);
+
+        zelva.turnRight(120);
+        zelva.move(100);
+
+    }
+
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
 
-        zofka.penDown();
-
-        //predni nohy
-        zofka.turnLeft(135);
-        zofka.move(50);
-        zofka.turnLeft(180);
-        zofka.move(50);
-
-        zofka.turnRight(90);
-        zofka.move(50);
-        zofka.turnLeft(180);
-        zofka.move(50);
-
-        zofka.turnRight(135);
-        zofka.move(200);
-
-        //zadni nohy
-        zofka.turnRight(135);
-        zofka.move(50);
-        zofka.turnLeft(180);
-        zofka.move(50);
-
-        zofka.turnRight(90);
-        zofka.move(50);
-        zofka.turnLeft(180);
-        zofka.move(50);
-
-        zofka.turnRight(45);
-        zofka.move(100);
-
-        zofka.turnLeft(90);
-        zofka.move(200);
-
-        zofka.turnLeft(90);
-        zofka.move(100);
-
-        //hlava
-        zofka.turnRight(120);
-        zofka.move(100);
-
-        zofka.turnRight(120);
-        zofka.move(100);
+        nakresliPrasatko(zofka);
         }
     }
 
